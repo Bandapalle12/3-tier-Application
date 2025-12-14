3aa538b5fed1885e37c3ef75a12bd0372b291017
Bandapalle <sourabh.v.bandapalle@accenture.com>
2025-12-14 16:59:06 +0530

    Added vpc, rds

--- variables.tf
+++ variables.tf
@@ -8,13 +8,3 @@ variable "project_name" {
   default = "three-tier-demo"
 }
 
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID where RDS will be created"
-  default = ""
-}
-
-variable "private_subnet_ids" {
-  type        = list(string)
-  description = "Private subnet IDs for RDS"
-}
