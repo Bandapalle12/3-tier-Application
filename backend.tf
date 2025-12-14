10f301073e78c2720e088843077894e419d0acda
Bandapalle <sourabh.v.bandapalle@accenture.com>
2025-12-14 19:09:53 +0530

    New account Added

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-demo777"
+    bucket         = "terraform-demo32"
     key            = "AWS/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
